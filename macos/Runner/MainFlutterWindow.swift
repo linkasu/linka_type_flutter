6ece3c8065109bbf9cc60fdf6c952eaab75ab387
Ivan Bakaidov <ivan@aacidov.ru>
2025-09-13 22:22:25 +0300

    Настроить развернутый режим окна для всех платформ
    - Windows: установить SW_MAXIMIZE для развертывания окна
    - macOS: использовать zoom() для развертывания окна
    - Linux: использовать gtk_window_maximize() для развертывания окна
    - Сохранены стандартные рамки и заголовки окон на всех платформах

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,10 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    
+    // Развернуть окно
+    self.zoom(nil)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -31,6 +31,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
+  window.Show();
+  ShowWindow(window.GetHandle(), SW_MAXIMIZE);
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {

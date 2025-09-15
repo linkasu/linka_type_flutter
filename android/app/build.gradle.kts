e28e88436f171ba7605edb137ffb31a8349bcc97
Ivan Alexandrovich Bakaidov <ivan@example.com>
2025-09-15 14:43:49 +0300

    Изменить applicationId на ru.ibakaidov.distypepro и обновить версию до 3.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "su.linka.type"
+    namespace = "ru.ibakaidov.distypepro"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "su.linka.type"
+        applicationId = "ru.ibakaidov.distypepro"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
